2a143bd8cf357962f333f32de0a11c05e61781bc
blackfireeagle <dianka_iusupova@outlook.com>
2024-11-24 15:34:37 +0300

    Update module_1.py

--- bulls_and_cows_project/module_1.py
+++ bulls_and_cows_project/module_1.py
@@ -1,1 +1,13 @@
-import random
+from main_final import BullsAndCowsGame
+
+def run_game():
+    '''
+    Точка входа для запуска игры. Создает окно и tkinter запускает приложение.
+    '''
+    root = tk.Tk()
+    game = BullsAndCowsGame(root) #Создание экземпляра игры
+    root.mainloop()
+
+# Проверка н прямой запуск модуля
+if __name__ == '__main__':
+    run_game()
